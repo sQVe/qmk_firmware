7f363d3e028f21c8327026965cb6dff09534bf69
Oskar Grunning <oskargrunning@gmail.com>
2020-02-18 13:25:33 +0100

    Uniform config between keymaps

--- keyboards/preonic/keymaps/sqve/config.h
+++ keyboards/preonic/keymaps/sqve/config.h
@@ -1,8 +1,10 @@
 #ifndef CONFIG_USER_H
 #define CONFIG_USER_H
-#include "../../config.h"
+#include "../../rev3/config.h"
 
 #define FORCE_NKRO
+#define IGNORE_MOD_TAP_INTERRUPT
 #define ONESHOT_TAP_TOGGLE 2
+#define TAPPING_TERM 200
 
 #endif
