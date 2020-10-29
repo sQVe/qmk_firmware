c1a52529a88425a9ccba477cd84cce086d0d97da
Oskar Grunning <oskargrunning@gmail.com>
2020-10-29 11:44:49 +0100

    Tweak configs

--- keyboards/ergodox_ez/keymaps/sqve/config.h
+++ keyboards/ergodox_ez/keymaps/sqve/config.h
@@ -1,10 +1,12 @@
 #ifndef CONFIG_USER_H
 #define CONFIG_USER_H
+
 #include "../../config.h"
 
+#undef  DEBOUNCE
+#define DEBOUNCE 15
+
+#undef  FORCE_NKRO
 #define FORCE_NKRO
-#define IGNORE_MOD_TAP_INTERRUPT
-#define ONESHOT_TAP_TOGGLE 2
-#define TAPPING_TERM 200
 
 #endif
