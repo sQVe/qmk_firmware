65b5ea2f0da626ad7b218b00541e37a181945e1b
Oskar Grunning <oskargrunning@gmail.com>
2021-02-25 19:16:02 +0100

    Update Preonic keymap

--- keyboards/preonic/keymaps/sqve/config.h
+++ keyboards/preonic/keymaps/sqve/config.h
@@ -1,14 +1,11 @@
+#pragma once
+
 #ifndef CONFIG_USER_H
 #define CONFIG_USER_H
+
 #include "../../rev3/config.h"
 
 #undef  FORCE_NKRO
 #define FORCE_NKRO
-
-#undef  IGNORE_MOD_TAP_INTERRUPT
-#define IGNORE_MOD_TAP_INTERRUPT
-
-#undef  TAPPING_TERM
-#define TAPPING_TERM 200
 
 #endif
