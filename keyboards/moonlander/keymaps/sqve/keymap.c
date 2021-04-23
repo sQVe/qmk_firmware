60cde120b8d2a147187b6ebecc1151ac61a201fa
Oskar Grunning <oskargrunning@gmail.com>
2021-04-23 15:18:47 +0200

    Tweak RGB animations

--- keyboards/moonlander/keymaps/sqve/keymap.c
+++ keyboards/moonlander/keymaps/sqve/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_moonlander(
     KC_ESC,   KC_1,          KC_2,          KC_3,   KC_4,     KC_5,     KC_DEL,                NO_SLSH,         KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     NO_PLUS,
     KC_TAB,   KC_Q,          KC_W,          KC_E,   KC_R,     KC_T,     LALT_T(KC_ENT),        LCTL_T(KC_SPC),  KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     NO_AM,
-    KC_BSPC,  KC_A,          KC_S,          KC_D,   KC_F,     KC_G,     OSL(MISC),             KC_MEH,          KC_H,     KC_J,     KC_K,     KC_L,     NO_AE,    LGUI_T(NO_OSLH),
+    KC_BSPC,  KC_A,          KC_S,          KC_D,   KC_F,     KC_G,     OSL(MISC),             KC_ESC,          KC_H,     KC_J,     KC_K,     KC_L,     NO_AE,    LGUI_T(NO_OSLH),
     KC_LSFT,  LCTL_T(KC_Z),  LALT_T(KC_X),  KC_C,   KC_V,     KC_B,                                             KC_N,     KC_M,     KC_COMM,  KC_DOT,   NO_MINS,  KC_RSFT,
     KC_LCTL,  KC_LGUI,       KC_LALT,       KC_UP,  KC_DOWN,            RGB_MOD,               RGB_TOG,                   KC_LEFT,  KC_RGHT,  KC_ALGR,  KC_DEL,   LT(MISC, NO_APOS),
                                                        KC_SPC,  KC_ESC, RGB_HUI,               RGB_SAI,  OSM(MOD_LGUI), LT(SYMB, KC_ENT)
@@ -104,7 +104,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  KC_NO,    KC_NO,    LCTL(LGUI(KC_LEFT)), LCTL(LGUI(KC_RGHT)),  KC_NO,    _______,             _______,  KC_F8,    KC_F10,   KC_F11,   S(KC_F11),  KC_F9,    KC_F12,
         _______,  KC_NO,    KC_NO,    KC_NO,               KC_NO,                KC_NO,    _______,             _______,  KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,    KC_NO,
         _______,  KC_NO,    KC_NO,    KC_MPRV,             KC_MPLY,              KC_MNXT,                                 KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,    _______,
-        _______,  _______,  _______,  _______,             _______,                        _______,             _______,            _______,  _______,  _______,    _______,  _______,                                                                     _______,  _______,  _______,             _______,  _______,  _______
+        _______,  _______,  _______,  _______,             _______,                        _______,             _______,            _______,  _______,  _______,    _______,  _______,
+                                                                       _______,  _______,  _______,             _______,  _______,  _______
     ),
 };
 
