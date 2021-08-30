9da75f40f5320bcfa6a466e99ba328a1a2674da6
Oskar Grunning <oskargrunning@gmail.com>
2021-08-30 08:47:47 +0200

    chore: align config and rules for keyboards

--- keyboards/moonlander/keymaps/sqve/config.h
+++ keyboards/moonlander/keymaps/sqve/config.h
@@ -5,12 +5,8 @@
 
 #include "../../config.h"
 
-#undef  FORCE_NKRO
 #define FORCE_NKRO
-
-#undef  IGNORE_MOD_TAP_INTERRUPT
 #define IGNORE_MOD_TAP_INTERRUPT
-
 #define ORYX_CONFIGURATOR
 
 // Disabled RGB modes.
